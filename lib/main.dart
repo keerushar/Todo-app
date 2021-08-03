80ff7839d48ef1bf71c56e2c38381f8510275158
keerushar <keerushar21@gmail.com>
2021-08-03 09:49:47 +0545

    splashscreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,33 @@
+import 'dart:async';
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:todo/widgets/homepage.dart';
 
-void main() => runApp(Todo());
+void main() => runApp(SplashScreen());
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: AnimatedSplashScreen(
+    splash: Image.asset('assets/images/splashscreen.png'),
+
+    nextScreen: Todo() ,
+    duration: 3000,
+    splashTransition: SplashTransition.decoratedBoxTransition,),
+      
+    );
+  }
+}
 
 class Todo extends StatelessWidget {
   @override
